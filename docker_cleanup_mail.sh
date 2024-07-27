4fcff995b8d0a1be6c51164edc91aafd3e457ef6
arpansahu <arpanrocks95@gmail.com>
2024-07-27 11:24:41 +0530

    updated docker cleanup mail.sh

--- docker_cleanup_mail.sh
+++ docker_cleanup_mail.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
 # Define the base directory where the .env file is located
-BASE_DIR="/root"
+BASE_DIR="/root/arpansahu-one-scripts"
+ENV_FILE="$BASE_DIR/.env"
 
-# Load environment variables from .env file if it exists
-if [ -f "$BASE_DIR/.env" ]; then
+# Load environment variables from .env file
+if [ -f "$ENV_FILE" ]; then
     echo "Loading environment variables from .env file..."
-    source "$BASE_DIR/.env"
+    export $(grep -v '^#' "$ENV_FILE" | xargs)
     echo "MAILJET_API_KEY: $MAILJET_API_KEY"
     echo "MAILJET_SECRET_KEY: $MAILJET_SECRET_KEY"
     echo "SENDER_EMAIL: $SENDER_EMAIL"
     echo "RECEIVER_EMAIL: $RECEIVER_EMAIL"
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
+# Check if required environment variables are set
+if [ -z "$MAILJET_API_KEY" ] || [ -z "$MAILJET_SECRET_KEY" ] || [ -z "$SENDER_EMAIL" ] || [ -z "$RECEIVER_EMAIL" ]; then
+    echo "One or more required environment variables (MAILJET_API_KEY, MAILJET_SECRET_KEY, SENDER_EMAIL, RECEIVER_EMAIL) are not set in the .env file"
+    exit 1
 fi
 
 # Email details
 LOG_FILE="/root/logs/docker_prune.log"
+
+# Check if the log file exists
+if [ ! -f "$LOG_FILE" ]; then
+    echo "Log file not found: $LOG_FILE"
+    exit 1
+fi
 
 # Read the log file contents
 BODY=$(cat "$LOG_FILE")
